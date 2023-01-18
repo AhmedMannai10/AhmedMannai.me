d6d3d81f6c26a0fbaf7d1788c02a47dac26d02f0
AhmedMannai10 <mannaiahmed7518@gmail.com>
2023-01-18 17:20:38 +0100

    UI : adding the aside section to the project post page

--- pages/projects/[slug]/index.js
+++ pages/projects/[slug]/index.js
@@ -34,14 +34,28 @@ const projectPost = () => {
             post && (
                 <>
                     <PostTitle title={post.title} publichedDate={post.publichedDate} />
-                    <div className="w-full max-w-none mb-4 border flex-1 border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 prose dark:prose-invert p-4 ">
+                    <div className="mx-auto flex flex-col-reverse justify-evenly lg:flex-row lg:gap-6 lg:px-0 mb-8">
+                        <div className="w-full max-w-none mb-4 border flex-1 border-gray-200 rounded-lg bg-gray-50 dark:bg-dark_secondary dark:border-gray-600 prose dark:prose-invert p-4 ">
+                            <div className="lg:max-w-3xl lg:mx-auto">
 
-                    <ReactMarkdown>{post.content}</ReactMarkdown>
+                                <ReactMarkdown>{post.content}</ReactMarkdown>
+                            </div>
+                        </div>
+                        <aside class="pt-10 top-14 lg:sticky lg:h-full w-full lg:w-80">
+                            <div className="space-y-6">
+                                <div className="prose dark:prose-dark max-w-none">
+                                    <div className="mb-4 cursor-pointer select-none overflow-auto max-h-[50vh]">
+                                        Hello There
+                                    </div>
+                                </div>
+                            </div>
+                        </aside>
                     </div>
                 </>)
 
         }
     </main>
+
 
 }
 
