3dea4c96c884f9dc5ebb771f31968576a0a89362
AhmedMannai10 <mannaiahmed7518@gmail.com>
2023-02-01 14:02:27 +0100

    UI: fixing title's bottom margin on posts

--- components/PostTitle.js
+++ components/PostTitle.js
@@ -3,9 +3,9 @@ import React from "react";
 const PostTitle = ({ title, publichedDate }) => {
     return (
         <header>
-            <div className="sm:pb-10 md:pb-14 space-y-2 text-center">
+            <div className="pb-10 md:mb-5 space-y-6 text-center">
                 <div className="w-full flex justify-center">
-                    <h1 className=" w-fit leading-9 break-words text-4xl sm:leading-10 md:text-5xl md:leading-14 bg-clip-text bg-gradient-to-r font-extrabold from-pink-600 to-sky-600 dark:from-pink-400 dark:to-sky-400 h1 leading-tighter text-center text-transparent tracking-tighter selection:text-gray-800 dark:selection:text-gray-200">
+                    <h1 className="mb-0 w-fit leading-9 break-words text-4xl sm:leading-10 md:text-5xl md:leading-14 bg-clip-text bg-gradient-to-r font-extrabold from-pink-600 to-sky-600 dark:from-pink-400 dark:to-sky-400 h1 leading-tighter text-center text-transparent tracking-tighter selection:text-gray-800 dark:selection:text-gray-200">
                         {title}
                     </h1>
                 </div>
@@ -13,7 +13,7 @@ const PostTitle = ({ title, publichedDate }) => {
                     <dl>
                         <div>
                             <dt className=" sr-only">Published On</dt>
-                            <dd className="text-lg font-medium leading-6 text-gray-700 dark:text-gray-200">
+                            <dd className="text-lg font-medium leading-6 text-gray-700 dark:text-gray-300">
                                 <time>{publichedDate}</time>
                             </dd>
                         </div>
