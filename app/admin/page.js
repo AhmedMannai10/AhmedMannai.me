d5d7257edb12c93f06cbc0381a10222c758ed57b
DrGhost <mannaiahmed7518@gmail.com>
2023-12-26 21:06:30 +0100

    fixing admin blog-post path (from 'blog-posts' to 'articles')

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,7 +1,7 @@
 'use client'
 import AdminDashboard from "../../components/AdminDashboard";
 import AuthCheck from "../../components/AuthCheck";
-import {useRouter} from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useState } from "react";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { firestore } from "../../lib/firebase";
@@ -13,92 +13,96 @@ import toast from "react-hot-toast";
 
 export default function AdminPage() {
 
-    return (
-        <main className="min-h-screen flex flex-col gap-2 mt-10 ">
-            <AuthCheck>
+  return (
+    <main className="min-h-screen flex flex-col gap-2 mt-10 ">
+      <AuthCheck>
 
-                <CreateNewPost />
-                <AdminDashboard />
+        <CreateNewPost />
+        <AdminDashboard />
 
-            </AuthCheck>
-        </main>
-    );
+      </AuthCheck>
+    </main>
+  );
 
 }
 
 
 function CreateNewPost() {
   const router = useRouter();
-    const [title, setTitle] = useState("");
-    const [isProject, setIsProject] = useState(false);
+  const [title, setTitle] = useState("");
+  const [isProject, setIsProject] = useState(false);
 
-    // Ensure slug is Url safe
-    const slug = encodeURI(kebabCase(title))
+  // Ensure slug is Url safe
+  const slug = encodeURI(kebabCase(title))
 
-    // Validate length
-    const isValid = title.length > 3 && title.length < 100;
-    let postType;
+  // Validate length
+  const isValid = title.length > 3 && title.length < 100;
+  let postType;
+  let firebasePostType;
 
-    if (isProject) {
-        postType = "projects";
-    } else {
-        postType = "blog-posts"
-    }
-
-    const createPost = async (e) => {
-        e.preventDefault();
-        const ref = doc(
-            firestore, postType, slug
-        );
-
-        console.log("sending");
-        // TIP: give all fields a default value here
-        const data = {
-            title,
-            slug,
-            content: "# hello world!",
-            published: false,
-            description: "# Desc Project",
-            createdAt: serverTimestamp(),
-            updatedAt: serverTimestamp(),
-            img: "https://firebasestorage.googleapis.com/v0/b/ahmed-mannai.appspot.com/o/mac.jpg?alt=media&token=4510df31-430d-44da-a990-a704075030bf",
-        };
-
-        await setDoc(ref, data);
-        toast.success("Post Created!");
+  if (isProject) {
+    postType = "projects";
+    firebasePostType = "projects";
+  } else {
+    postType = "articles"
+    firebasePostType = "blog-posts"
+  }
 
 
-        router.push(`/admin/${postType}/${slug}`);
+  const createPost = async (e) => {
+    e.preventDefault();
+    const ref = doc(
+      firestore, firebasePostType, slug
+    );
+
+    console.log("sending");
+    // TIP: give all fields a default value here
+    const data = {
+      title,
+      slug,
+      content: "# hello world!",
+      published: false,
+      description: "# Desc Project",
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      img: "https://firebasestorage.googleapis.com/v0/b/ahmed-mannai.appspot.com/o/mac.jpg?alt=media&token=4510df31-430d-44da-a990-a704075030bf",
     };
 
-    return (
-        <div className="flex flex-col ">
+    await setDoc(ref, data);
+    toast.success("Post Created!");
 
-            <form onSubmit={createPost} >
-                <input
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    placeholder="My Awesome Article!"
-                    className="border-2 rounded-md "
-                />
-                <p>
-                    <strong>Slug:</strong>
-                    {slug}
-                </p>
-                <fieldset>
-                    <input name="published" type="checkbox" onClick={() => { setIsProject(!isProject) }} />
-                    <label>Article about a project</label>
-                </fieldset>
-                <button
-                    type="submit"
-                    disabled={!isValid}
-                    className="rounded-full bg-blue-700 text-white shadow-md hover:shadow-none px-2 mt-2"
-                >
-                    Submit
-                </button>
 
-            </form>
+    router.push(`/admin/${postType}/${slug}`);
+  };
 
-        </div>
-    );
+  return (
+    <div className="flex flex-col ">
+
+      <form onSubmit={createPost} >
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="My Awesome Article!"
+          className="border-2 rounded-md "
+        />
+        <p>
+          <strong>Slug:</strong>
+          {slug}
+        </p>
+        <fieldset>
+          <input name="published" type="checkbox" onClick={() => { setIsProject(!isProject) }} />
+          <label>Article about a project</label>
+        </fieldset>
+        <button
+          type="submit"
+          disabled={!isValid}
+          className="rounded-full bg-blue-700 text-white shadow-md hover:shadow-none px-2 mt-2"
+        >
+          Submit
+        </button>
+
+      </form>
+
+    </div>
+  );
 }
--- components/BlogCard.js
+++ components/BlogCard.js
@@ -3,39 +3,38 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function BlogCard({ img, title, desc, link }) {
-    return (
-        <Link href={`/blog/${link} `}>
-                          
-            
-        <div className="max-w-sm bg-secondary max-h-fit
-              dark:bg-dark_secondary rounded-lg shadow 
+  return (
+    <Link href={`/blog/${link} `}>
+
+
+      <div className="max-w-sm bg-secondary max-h-fit
+              dark:bg-dark_secondary 
                 rounded-md shadow-lg hover:-translate-y-[4px] duration-300 ease-out overflow-hidden
         ">
 
         <div
-                        className="relative min-h-[250px] "
-                    >
-        <Image
-        loading="lazy"
-        src={img}
-        alt={`${title}  project image`}
-        layout='fill'
-        objectFit="cover"
-        />
+          className="relative  min-h-[230px]  "
+        >
+          <Image
+            src={img}
+            alt={`${title}  project image`}
+            objectFit="cover"
+            layout="fill"
+          />
         </div>
         <div className="p-5">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{desc}</p>
-        <div  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{desc}</p>
+          <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 
         focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">
             Read more
             <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
             </svg>
+          </div>
         </div>
-        </div>
- 
-        </div>
-                        </Link>
-    );
+
+      </div>
+    </Link>
+  );
 }
